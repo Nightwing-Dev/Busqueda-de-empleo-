eb651ff43476cd14562ca6af50af6603dd9f1942
Nightwing-Dev <diego77andre77@gmail.com>
2022-06-30 22:47:52 -0500

    maketacion y estilos del portal principal

--- portal-empleo/src/app/app.component.spec.ts
+++ portal-empleo/src/app/app.component.spec.ts
@@ -33,3 +33,4 @@ describe('AppComponent', () => {
     expect(compiled.querySelector('.content span')?.textContent).toContain('portal-empleo app is running!');
   });
 });
+
--- portal-empleo/src/app/app.module.ts
+++ portal-empleo/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
